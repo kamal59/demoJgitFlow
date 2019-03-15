67232a8e1641582608427223784035b970e3e10e
kamal <kamal.machto@gmail.com>
2019-03-15 11:08:45 +0100

    first commit

---
+++ src/test/java/com/example/demoJgitFlow/DemoJgitFlowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoJgitFlow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoJgitFlowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
